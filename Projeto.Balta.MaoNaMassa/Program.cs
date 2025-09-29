5cc5af3280f015476523da5f5f640c5f18bc0e98
Arruda98 <jeffersonarruda98@gmail.com>
2025-09-29 13:30:40 -0300

    fist repository the project

---
+++ Projeto.Balta.MaoNaMassa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Balta.ContentContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+        }
+    }
+}
